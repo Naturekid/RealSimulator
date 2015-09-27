003e5cef6fecef7a6b1f46e241f82b0463084a30
Naturekid <caibyyxf@sina.cn>
2015-09-27 09:58:07 +0800

    修改成非阻塞状态，超时接收不到服务器端的回复时进入下一次请求。

--- ntp_client.c
+++ ntp_client.c
@@ -1,167 +1,293 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+
 #include <stdlib.h>
 
+
+
 #include <string.h>
+
 #include <sys/socket.h>
+
 #include <netinet/in.h>
+
 #include <arpa/inet.h>
+
 #include <netdb.h>
+
 #include <sys/time.h>
 
-int out_port=6789;
-int in_port = 6788;
 
-int my_difftime(struct timeval  *t1,struct timeval  *t2);
+
+//#define DEBUG
+//若记录同步过程
+//#define RECORD
+
+#define WSAECONNABORTED 10053
+
+
+
+int out_port=6789;//发送端口
+
+int in_port = 6788;//接收端口
+
+int sleep_time = 8;
+
+
+struct timeval tv_out;//接收超时的时间变量
+
+#ifdef RECORD
+FILE *log_file;//log文件
+#endif
+
+//计算两个时间之差
+//int my_difftime(struct timeval  *t1,struct timeval  *t2);
+//将从服务器端收到的时间序列拆分为本地发送时间，远程接收时间，远程发送时间
 void parse(char *str,double *lt1,double *rt1,double *rt2);
+
+
 
 int main(int argc, char** argv) {
 
+
+	//如果未输入服务器IP则不执行
 	if(argc<=1)
 	{
+
 		printf("Please input server IP\n");
+
 		return 0;
+
 	}
+
 	printf("Try to connect Server:%s\n",argv[1]);
+	
+	if(argc==3)
+	{
+		
+		sleep_time =atoi(argv[2]);
+		printf("Sleep Time is %d\n",sleep_time);
+	}	
 
-    	int socket_descriptor; //套接口描述字
-	int iter=0;
-    	char buf[80];
-    	struct sockaddr_in address;//处理网络通信的地址
+#ifdef RECORD
+	//打开log文件
+	log_file = fopen("./log","a+");
+#endif
 
-    	bzero(&address,sizeof(address));
-    	address.sin_family=AF_INET;
-    	address.sin_addr.s_addr=inet_addr(argv[1]);//这里不一样
-    	address.sin_port=htons(out_port);
 
+
+	int socket_descriptor; //套接口描述字
+
+	int iter=0;//计数器
+
+    char buf[80];//缓存区
+
+	//发送的网络地址
+    struct sockaddr_in address;//处理网络通信的地址
+
+	bzero(&address,sizeof(address));
+	address.sin_family=AF_INET;//指定协议族
+	address.sin_addr.s_addr=inet_addr(argv[1]);//这里不一样，指定服务器端
+	address.sin_port=htons(out_port);//指定出口端口
 
     //创建一个 UDP socket
+    socket_descriptor=socket(AF_INET,SOCK_DGRAM,0);//IPV4  SOCK_DGRAM 数据报套接字（UDP协议）
+	
+	char message[256];//缓存从服务器端接收到的信息
 
-    	socket_descriptor=socket(AF_INET,SOCK_DGRAM,0);//IPV4  SOCK_DGRAM 数据报套接字（UDP协议）
-
-//get
-	char message[256];
 	int sin_len;
-    struct sockaddr_in sin;
-    bzero(&sin,sizeof(sin));
-    sin.sin_family=AF_INET;
-    sin.sin_addr.s_addr=htonl(INADDR_ANY);
-    sin.sin_port=htons(in_port);
+	//接收地址
+	struct sockaddr_in sin;
+	bzero(&sin,sizeof(sin));
+	sin.sin_family=AF_INET;
+	sin.sin_addr.s_addr=htonl(INADDR_ANY);
+	sin.sin_port=htons(in_port);//接收地址
     sin_len=sizeof(sin);
+	
+	//为socket设定超时时间
+
+	//超时时间为6s 
+    tv_out.tv_sec = 2;//等待6秒
+    tv_out.tv_usec = 0;
+
+int nTimeout=1000;
+	//setsockopt(socket_descriptor,SOL_SOCKET,SO_RCVTIMEO,(char *)&nTimeout, sizeof(int));//第二个参数为指定选项代码，为IPV4
+
+	setsockopt(socket_descriptor,SOL_SOCKET,SO_RCVTIMEO,&tv_out, sizeof(struct timeval));//第二个参数为指定选项代码，为IPV4
+	//绑定
     bind(socket_descriptor,(struct sockaddr *)&sin,sizeof(sin));
 
+	
+	//发送次数
+    int send_max = 30;
 
-    int send_max = 512;
+
+
 
     for(iter=0;iter<=send_max;iter++)
+
     {
+
 #ifdef DEBUG
 	printf("Send Query %d\n",iter);
 #endif
-	struct timeval  *send_time = (struct timeval  *)malloc(sizeof(struct timeval));
 	
-         /*
-         * sprintf(s, "%8d%8d", 123, 4567); //产生：" 123 4567" 
-         * 将格式化后到 字符串存放到s当中
-         */
+	//发送时间
+	struct timeval  *send_time = (struct timeval  *)malloc(sizeof(struct timeval));
+	struct timeval  *recv_time = (struct timeval  *)malloc(sizeof(struct timeval));
+	struct timeval  *new_time = (struct timeval  *)malloc(sizeof(struct timeval));
 
+	//获得发送时间，精确到微秒
 	if(gettimeofday(send_time,NULL)==-1)//error
+
 	{
 		free(send_time);
 		continue;
-		
 	}
+
 	else{
+
+		//将timeval结构的时间转换为字符串发送
 		unsigned int second = send_time->tv_sec;
+
 		unsigned int usecond = send_time->tv_usec;
-		double utime = (double)second+ usecond*1.0/100000;
+
+		double utime = (double)second+ usecond*1.0/1000000;
+
+//		printf("%f\n",utime);
+
 		sprintf(buf,"%f",utime);
-		//sprintf(buf,"%s ",send_time);
+
 	}
-        //sprintf(buf,"data packet with ID %d\n",iter);
-       
-        /*int PASCAL FAR sendto( SOCKET s, const char FAR* buf, int len, int flags,const struct sockaddr FAR* to, int tolen);　　
-         * s：一个标识套接口的描述字。　
-         * buf：包含待发送数据的缓冲区。　　
-         * len：buf缓冲区中数据的长度。　
-         * flags：调用方式标志位。　　
-         * to：（可选）指针，指向目的套接口的地址。　
-         * tolen：to所指地址的长度。  
-　　      */
-        sendto(socket_descriptor,buf,sizeof(buf),0,(struct sockaddr *)&address,sizeof(address));
+
+    /*int PASCAL FAR sendto( SOCKET s, const char FAR* buf, int len, int flags,const struct sockaddr FAR* to, int tolen);　　
+    	* s：一个标识套接口的描述字。　
+		* buf：包含待发送数据的缓冲区。
+        * len：buf缓冲区中数据的长度。
+        * flags：调用方式标志位。
+        * to：（可选）指针，指向目的套接口的地址。
+        * tolen：to所指地址的长度。  
+	*/
+
+	//发送给客户端
+    sendto(socket_descriptor,buf,sizeof(buf),0,(struct sockaddr *)&address,sizeof(address));
 	
 	free(send_time);
 
-	recvfrom(socket_descriptor,message,sizeof(message),0,(struct sockaddr *)&sin,&sin_len);
-
-	struct timeval  *recv_time = (struct timeval  *)malloc(sizeof(struct timeval));
+	
+	//监听来自服务器的消息
+	int result = recvfrom(socket_descriptor,message,sizeof(message),0,(struct sockaddr *)&sin,&sin_len);
+	if(result == -1){
+		continue ;
+	}
+	
+	//获得接收时间
 	gettimeofday(recv_time,NULL);
 
-	double lt2 = (double)recv_time->tv_sec + recv_time->tv_usec *1.0/1000000;
+	//分别为服务器的接收时间，服务器发送时间，本地发送时间
 	double rt1,rt2,lt1;
+	//处理为double型数据
 	parse(message,&lt1,&rt1,&rt2);
+	
+
+
+	//将接收时间转换为double型
+	double lt2 = (double)recv_time->tv_sec + recv_time->tv_usec *1.0/1000000;
+//	printf("%f\n",lt2);
 
 #ifdef DEBUG
 	printf("%s\n",message);
 	printf("%f %f %f %f\n",lt1,lt2,rt1,rt2);
 #endif
 
-	
-	
+	//计算本机和服务器的时间差
+	double delay = (lt2-lt1) - (rt2-rt1);
+//	printf("%f\n",delay);
 	double offset = ((rt1-lt1)+(rt2-lt2))/2;
-	struct timeval  *new_time = (struct timeval  *)malloc(sizeof(struct timeval));
+
+	//获得当前时间
+
 	gettimeofday(new_time,NULL);
+
 	double nt = (double)new_time->tv_sec + new_time->tv_usec *1.0/1000000;
-	
+
 	//set new time
-	
+	//调整到新时间
 	nt = nt + offset;
 	long i_nt = (unsigned int)nt;
-	
-	new_time->tv_sec = i_nt;
-	
-	new_time->tv_usec = (nt-i_nt) * 1000000;
+	new_time->tv_sec = i_nt;//秒
+	new_time->tv_usec = (nt-i_nt) * 1000000;//微秒
 
+	//设置为新时间
 	settimeofday(new_time, NULL);
-
-	gettimeofday(new_time,NULL);
-
-#ifdef DEBUG
-	printf("%f %f %d %d\n",offset,nt,(long)new_time->tv_sec,(long)new_time->tv_usec);
-#endif
 	
-	free(new_time);
+//	gettimeofday(new_time,NULL);
 
+//#ifdef DEBUG
+//	printf("%f %f %f %f %f %f\n",offset,delay,lt1,rt1,rt2,lt2);
+	//printf("%f %f %d %d\n",offset,nt,(long)new_time->tv_sec,(long)new_time->tv_usec);
+//	printf("%d	new_time:%lf	diff_time:%lf\n",iter,nt,offset);
+	printf("%lf\n",offset);
+//#endif
+
+#ifdef RECORD
+	char log[128];
+	memset(log,0,strlen(log));
+	
+	//sprintf("send_time:%lf	server_recv:%lf	server_send:%lf	recv_time:%lf	new_time:%lf\n",lt1,rt1,rt2,lt2,nt);
+	sprintf(log,"new_time:%lf	diff_time:%lf\n",nt,offset);
+	fputs(log,log_file);
+	
+#endif
+
+	//释放
+	free(new_time);
 	free(recv_time);
 	
-	sleep(8); 
+	//每8秒运行一次
+	sleep(sleep_time); 
     }
+
 
     sprintf(buf,"stop\n");
     sendto(socket_descriptor,buf,sizeof(buf),0,(struct sockaddr *)&address,sizeof(address));//发送stop 命令
-    close(socket_descriptor);
+    //关闭socket
+	close(socket_descriptor);
     printf("Messages Sent,terminating\n");
 
-    exit(0);
+	//关闭文件
+#ifdef RECORD
+	fclose(log_file);
+#endif
 
+    exit(0);
+	
     return (EXIT_SUCCESS);
+
 }
+
+
 
 void parse(char *str,double *lt1,double *rt1,double *rt2)
 {
 	int i = 0;
+
 	char tmp[30];
+
 	char sec[20];
+
 	char usec[20];
-//lt1
+
+	//lt1
 	for(i=0;str[i]!=' ';i++)
 	{
 		tmp[i] = str[i];
 	}
 	tmp[i] = '\n';
+
+	//转为double型
 	*lt1 = atof(tmp);
 	
-	
-//rt1
+	//rt1
 	i++;
 	int j;
 	for(j=0;str[i]!=' ';)
@@ -173,7 +299,8 @@ void parse(char *str,double *lt1,double *rt1,double *rt2)
 
 	tmp[j] = '\n';
 	*rt1 = atof(tmp);
-	
+
+	//rt2
 	i++;
 	int k;
 	for(k=0;str[i]!='\n';)
@@ -186,60 +313,3 @@ void parse(char *str,double *lt1,double *rt1,double *rt2)
 	tmp[k] = '\n';
 	*rt2 = atof(tmp);
 }
-/*
-void toTimeval(char* time,struct timeval* tv)
-{
-	int i = 0;
-	char sec[20];
-	char usec[20];
-	for(i=0;time[i]!='.';i++)
-	{
-		sec[i] = time[i];
-	}
-	sec[i] = '\n';
-	i++;
-	int j;
-	for(j=i;time[j]!='\n';j++)
-	{
-		usec[j-i] = time[j];
-	}
-
-	usec[j-i] = '\n';
-
-	struct timeval tmp;
-	tmp.tv_sec = atoi(sec);
-	tmp.tv_usec = atoi(usec);
-}
-
-void newTime(struct timeval  *lt1 , struct timeval  *lt2 , struct timeval  *rt1 , struct timeval  *rt2, struct timeval  *newtime)
-{
-	
-
-//Delay=(lt2-lt1)-(rt2-rt1)
-//offset=((rt1-lt1)+(rt2-lt2))/2
-
-	long sec_offset =( (rt1->tv_sec - lt1->tv_sec) + (rt2->tv_sec - lt2->tv_sec) )/2;
-	long sec_offset =( (rt1->tv_usec - lt1->tv_usec) + (rt2->tv_usec - lt2->tv_usec) )/2;
-	
-	if(sec_offset < 0)
-	{
-		
-	}
-	
-	newtime->sec = lt2->sec;
-	newtime->usec = lt2->usec
-	
-	
-	
-	
-	
-}
-*/
-/*
-struct timeval* my_difftime(struct timeval  *t1 , struct timeval  *t2)
-{
-	unsigned int usec = 
-	return;
-}
-*/
-
--- ntp_server.c
+++ ntp_server.c
@@ -8,17 +8,19 @@
 #include <netdb.h>
 #include <sys/time.h>
 
-int in_port=6789;
-int out_port= 6788;
+//#define DEBUG
+
+int in_port=6789;//接收端口
+int out_port= 6788;//发送端口
 
 int main(int argc, char** argv) {
 
     int sin_len;
-    char message[256];
-    char buf[256];
+    char message[256];//接收信息
+    char buf[256];//发送缓冲区
 
-    int socket_descriptor;
-    struct sockaddr_in sin;
+    int socket_descriptor;//socket描述符
+    struct sockaddr_in sin;//接收地址
     printf("Waiting for data form sender \n");
 
     bzero(&sin,sizeof(sin));
@@ -28,65 +30,72 @@ int main(int argc, char** argv) {
     sin_len=sizeof(sin);
 
     socket_descriptor=socket(AF_INET,SOCK_DGRAM,0);
-    bind(socket_descriptor,(struct sockaddr *)&sin,sizeof(sin));
+    bind(socket_descriptor,(struct sockaddr *)&sin,sizeof(sin));//绑定端口
+
+
 
     while(1)
     {
-	struct timeval  *recv_time = (struct timeval  *)malloc(sizeof(struct timeval));
-	struct timeval  *send_time = (struct timeval  *)malloc(sizeof(struct timeval));
-
+		struct timeval  *recv_time = (struct timeval  *)malloc(sizeof(struct timeval));//接收时间
+		struct timeval  *send_time = (struct timeval  *)malloc(sizeof(struct timeval));//发送时间
+		
+		//等待接收请求
         recvfrom(socket_descriptor,message,sizeof(message),0,(struct sockaddr *)&sin,&sin_len);
 	
-	if(gettimeofday(recv_time,NULL)==-1)//error
-	{
-		free(send_time);
-		free(recv_time);
-		continue;
+		//获取接收到请求的时间
+		if(gettimeofday(recv_time,NULL)==-1)//error
+		{
+			free(send_time);
+			free(recv_time);
+			continue;
+		}
 		
-	}
-	
+#ifdef DEBUG
         printf("Response from server:%s\n",message);
+#endif
+		
         if(strncmp(message,"stop",4) == 0)//接受到的消息为 “stop”
         {
-
             printf("Sender has told me to end the connection\n");
             break;
         }
 
-	struct sockaddr_in address;//处理网络通信的地址
+		struct sockaddr_in address;//处理网络通信的地址
 
-   	bzero(&address,sizeof(address));
-    	address.sin_family=AF_INET;
-   	address.sin_addr.s_addr=sin.sin_addr.s_addr;//这里不一样
-   	address.sin_port=htons(out_port);
+		bzero(&address,sizeof(address));
+    		address.sin_family=AF_INET;
+		address.sin_addr.s_addr=sin.sin_addr.s_addr;//这里不一样
+		address.sin_port=htons(out_port);
+		
+		//将时间转换为double型
+		unsigned int r_sec = recv_time->tv_sec;
+		unsigned int r_usec = recv_time->tv_usec;
+		double r_utime = (double)r_sec + r_usec*1.0/1000000;
 
-	if(gettimeofday(send_time,NULL)==-1)//error
-	{
-		free(send_time);
-		free(recv_time);
-		continue;
-	}
+		//获得发送时间
+		if(gettimeofday(send_time,NULL)==-1)//error
+		{
+			free(send_time);
+			free(recv_time);
+			continue;
+		}
 
-	unsigned int r_sec = recv_time->tv_sec;
-	unsigned int r_usec = recv_time->tv_usec;
-	unsigned int s_sec = send_time->tv_sec;
-	unsigned int s_usec = send_time->tv_usec;
-	double r_utime = (double)r_sec + r_usec*1.0/1000000;
-	double s_utime = (double)s_sec + s_usec*1.0/1000000;
+		//将时间转换为double型
+		unsigned int s_sec = send_time->tv_sec;
+		unsigned int s_usec = send_time->tv_usec;
+		double s_utime = (double)s_sec + s_usec*1.0/1000000;
 
-	sprintf(buf,"%s %f %f\n",message,r_utime,s_utime);
+		sprintf(buf,"%s %f %f\n",message,r_utime,s_utime);
 
-	printf("%s",buf);
+		//printf("%s",buf);
 
-sleep(1);
+		//给客户端发送回复信息
+		sendto(socket_descriptor,buf,sizeof(buf),0,(struct sockaddr *)&address,sizeof(address));
 
-	sendto(socket_descriptor,buf,sizeof(buf),0,(struct sockaddr *)&address,sizeof(address));
-
-
-	printf("*********%d**********\n",address.sin_addr.s_addr);
-	
-	
-	
+#ifdef DEBUG
+		printf("*********%d**********\n",address.sin_addr.s_addr);
+#endif
+		
     }
 
     close(socket_descriptor);
